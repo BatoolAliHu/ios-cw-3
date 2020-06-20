09a05e791a09665f2b73f7585f5db6bda78580c4
BatoolAliHussain <66515156+BatoolAliHu@users.noreply.github.com>
2020-06-20 20:26:47 +0300

    done
    تم الانتهاء

---
+++ ios-cw3-p1.playground/Contents.swift
@@ -0,0 +1,50 @@
+
+var number1 = 4
+var number2 = 10
+
+// Add
+
+func add(a:Int, b: Int) -> Int
+{
+    return a + b
+}
+
+var functionCall = add(a: number1, b: number2)
+print("\(number1) + \(number2) = \(functionCall)")
+
+//Multiplay
+
+func multipaly(a:Int, b: Int) -> Int
+{
+    return a * b
+}
+
+var functionCallMultiplay = multipaly(a: number1, b: number2)
+print("\(number1) * \(number2) = \(functionCall)")
+
+// Divide
+
+func divide (a:Int, b: Int) -> Int
+{
+    return a / b
+}
+
+var functionCalldivide = divide(a: number1, b: number2)
+print("\(number1) / \(number2) = \(functionCall)")
+
+// Subtract
+
+func subtract(a:Int, b: Int) -> Int
+{
+    return a * b
+}
+
+var functionCallSubtract = subtract(a: number1, b: number2)
+print("\(number1) - \(number2) = \(functionCall)")
+
+
+
+
+
+
+
